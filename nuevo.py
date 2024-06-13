d74778785209948d40df9399219f7714e4de40e4
FranchiquiJ <167950323+FranchiquiJ@users.noreply.github.com>
2024-06-13 15:58:15 +0200

    c 	modified:   nuevo.py 	modified:   python/test/transcribe_scp.py 	modified:   requirements.txt

--- nuevo.py
+++ nuevo.py
@@ -1,12 +1,7 @@
 import vosk
 import speech_recognition as sr
-import vosk.kaldi_asr as kaldi_asr
+from vosk import Model, KaldiRecognizer
 
-
-
-
-def listen_to_speech():
-    r = sr.Recognizer() 
 
 def listen_to_speech(model_path):
     """
@@ -19,9 +14,11 @@ def listen_to_speech(model_path):
         str: The recognized text if successful, or an error message if recognition fails.
     """
 
-    model = vosk.Model(model_path)
-    recognizer = vosk.KaldiRecognizer(model)
+    model = Model(model_path)
+    recognizer = KaldiRecognizer(model, 16000)  # Sample rate (usually 16000 Hz)
+
     with sr.Microphone() as source:
+        print("Speak your request:")
         audio = recognizer.get_activation()
         while audio:
             data = recognizer.recognize(audio)
@@ -36,28 +33,13 @@ def listen_to_speech(model_path):
 
 
 # Example usage
-model_path = "D:/vosk-model-es-0.42/"  # Replace with your model path
+model_path = "D:/vosk-model-es-0.42/"  # Replace with your actual path
 recognized_text = listen_to_speech(model_path)
+
 if recognized_text:
     # Process the recognized text as needed
     print(f"Processing recognized text: {recognized_text}")
 else:
     print("Speech recognition failed.")
-    
-    
-model_path = "D:/vosk-model-es-0.42/"  # Replace with your actual path
-model = vosk.Model(model_path)
 
-recognizer = kaldi_asr.KaldiRecognizer(model)    
-    
-    
-with sr.Microphone() as source:
-    audio = recognizer.get_activation()
-    while audio:
-        data = recognizer.recognize(audio)
-        if data[0] is not None:
-            text = data[0][0]
-            print(f"You said: {text}")
-            # Process the recognized text as needed
-        audio = recognizer.get_activation()
     
